33f1cf2b84f188de61102f7845786c1e2a926986
Joseph_d <joseph_d@etna-alternance.net>
2024-11-04 20:06:25 +0100

    start

---
+++ back_appstock/src/main/java/com/example/movestock/demo/MoveStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.movestock.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveStockApplication.class, args);
+	}
+
+}
